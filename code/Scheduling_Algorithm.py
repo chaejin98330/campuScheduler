a1f8f4016fee334c22ef88c83aec18083bab941f
ansol4328 <woosol0125@gmail.com>
2022-11-16 22:07:01 +0900

    restore tmp file

--- code/Scheduling_Algorithm.py
+++ code/Scheduling_Algorithm.py
@@ -1,10 +1,34 @@
 from Network_Flow import *
 from Scheduling_Class import *
 
-def scheduling(student_list, schedule_list):
+# 입력 파싱 후 호출할 함수
+def scheduling(student_list, schedule):
+    if not schedule.equality_flag:
+        return all_scheduling(student_list)
+    else:
+        return equal_scheduling(student_list, schedule)
+
+def equal_scheduling(student_list, schedule):
     # make graph
 
     # scheduling using dinic
 
     # save result
     None
+
+def all_scheduling(student_list):
+    student_num = len(student_list)
+    rn = len(student_list[0].pos)
+    cn = len(student_list[0].pos[0])
+    # 학생 시간표 형태와 동일한 형태의 결과물
+    # 해당 시간에 모두가 공강이면 True로 나타난다.
+    ret = [[] for i in range(rn)]
+    for r in ret:
+        for j in range(cn):
+            r.append(True)
+    for i in range(student_num):
+        for j in range(rn):
+            for k in range(cn):
+                if not student_list[i].pos[j][k]:
+                    ret[i][j] = False
+    return ret
--- code/Scheduling_Class.py
+++ code/Scheduling_Class.py
@@ -1,23 +1,19 @@
 class Student:
     # name = 이름
     # sid = 학번
-    # mini = 최소 횟수
-    # maxi = 최대 횟수
     # pos = 활동 가능 시간 리스트
-    def __init__(self, name, sid, mini, maxi, pos):
+    def __init__(self, name, sid, pos):
         self.name = name
         self.sid = sid
-        self.mini = mini
-        self.maxi = maxi
         self.pos = pos
 
 class Schedule:
+    # equality_flag = 균등 분배 여부
     # mini = 최소 필요 인원
     # maxi = 최대 필요 인원
-    # st = 시작 시간
-    # fn = 종료 시간
-    def __init__(self, mini, maxi, st, fn):
+    # need = 전체 시간대 정수 2차원 배열
+    def __init__(self, equality_flag, mini, maxi, need):
+        self.equality_flag = equality_flag
         self.mini = mini
         self.maxi = maxi
-        self.st = st
-        self.fn = fn
+        self.need = need
