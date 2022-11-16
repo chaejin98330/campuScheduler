114f8c0f207a75655bb1098828fcbffb60088206
ansol4328 <woosol0125@gmail.com>
2022-11-16 22:57:34 +0900

    modify scheduling class

--- code/Scheduling_Class.py
+++ code/Scheduling_Class.py
@@ -1,7 +1,7 @@
 class Student:
     # name = 이름
     # sid = 학번
-    # pos = 활동 가능 시간 리스트
+    # pos = 활동 가능 시간을 30분 단위로 자른 표
     def __init__(self, name, sid, pos):
         self.name = name
         self.sid = sid
@@ -9,11 +9,13 @@ class Student:
 
 class Schedule:
     # equality_flag = 균등 분배 여부
+    # unit_time = 단위 시간
     # mini = 최소 필요 인원
     # maxi = 최대 필요 인원
-    # need = 전체 시간대 정수 2차원 배열
-    def __init__(self, equality_flag, mini, maxi, need):
+    # need = 전체 시간대 (시작 분, 끝 분) 튜플의 리스트
+    def __init__(self, equality_flag, unit_flag, mini, maxi, need):
         self.equality_flag = equality_flag
+        self.unit_flag = unit_flag
         self.mini = mini
         self.maxi = maxi
         self.need = need
