4ebbf461172aa81cf283a39e91d740afef3704c3
NAHOHYUN-SKKU <50166462+NAHOHYUN-SKKU@users.noreply.github.com>
2022-11-19 23:51:51 +0900

    Update Scheduling_Output.py

--- code/Scheduling_Output.py
+++ code/Scheduling_Output.py
@@ -3,7 +3,7 @@ import math
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Alignment, PatternFill
 
-def printExcel(result, student_list, group_name):
+def printExcel(equality_flag, result, student_list, group_name):
     wb = openpyxl.Workbook()
     sheet = wb.active
     grey_color = PatternFill(start_color='e6e6e6', end_color='e6e6e6', fill_type='solid')
@@ -30,31 +30,35 @@ def printExcel(result, student_list, group_name):
     sheet['E1'] = "Thu"
     sheet['F1'] = "Fri"
 
-    for i in range(len(result)):
-        name = student_list[i].name
-        id = student_list[i].sid
-        for j in range(len(result[i])):
-            start = result[i][j][0]
-            end = result[i][j][1]
-            day = math.floor(start / (24*60))
-            start = (start - day * 24 * 60)/60
-            end = (end - day * 24 * 60)/60
+    if(equality_flag):
+        for i in range(len(result)):
+            name = student_list[i].name
+            id = student_list[i].sid
+            for j in range(len(result[i])):
+                start = result[i][j][0]
+                end = result[i][j][1]
+                day = math.floor(start / (24*60))
+                start = (start - day * 24 * 60)/60
+                end = (end - day * 24 * 60)/60
             
-            start_r = (math.floor(start) - 8) * 2
-            if not float(start).is_integer():
-                start_r += 1
-            end_r = (math.floor(end) - 8) * 2 - 1
-            if not float(end).is_integer():
-                end_r += 1
+                start_r = (math.floor(start) - 8) * 2
+                if not float(start).is_integer():
+                    start_r += 1
+                end_r = (math.floor(end) - 8) * 2 - 1
+                if not float(end).is_integer():
+                    end_r += 1
 
-            sheet.merge_cells(start_row=start_r, start_column=day+2, end_row=end_r, end_column=day+2)
-            if sheet.cell(row=start_r, column=day+2).value == None:
-                sheet.cell(row=start_r, column=day+2).value = name + "(" + str(id) + ")"
-            else:
-                sheet.cell(row=start_r, column=day+2).value += ",\n" + name + "(" + str(id) + ")"
-
+                sheet.merge_cells(start_row=start_r, start_column=day+2, end_row=end_r, end_column=day+2)
+                if sheet.cell(row=start_r, column=day+2).value == None:
+                    sheet.cell(row=start_r, column=day+2).value = name + "(" + str(id) + ")"
+                else:
+                    sheet.cell(row=start_r, column=day+2).value += ",\n" + name + "(" + str(id) + ")"
+    elif(not equality_flag):
+        for i in range(26):
+            for j in range(5):
+                if result[i][j]:
+                    sheet.cell(row=i+2, column=j+2).value = "Possible"
     for row in sheet.rows:
-        for cell in row:
-            cell.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
-
+            for cell in row:
+                cell.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
     wb.save('../data/' + group_name + '/' + group_name + '.xlsx')
